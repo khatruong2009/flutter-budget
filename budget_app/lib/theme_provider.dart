020f07edcc851347de66bbb0f79cae89494af090
Kha Truong <64438356+khatruong2009@users.noreply.github.com>
2023-08-13 00:24:08 -0700

    use shared preferences to save mode

--- budget_app/lib/theme_provider.dart
+++ budget_app/lib/theme_provider.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
   ThemeMode _themeMode = ThemeMode.system;
 
   ThemeMode get themeMode => _themeMode;
 
-  void toggleTheme() {
+  ThemeProvider() {
+    _loadTheme();
+  }
+
+  void _loadTheme() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? themeString = prefs.getString('themeMode');
+    if (themeString != null) {
+      if (themeString == 'light') {
+        _themeMode = ThemeMode.light;
+      } else if (themeString == 'dark') {
+        _themeMode = ThemeMode.dark;
+      } else {
+        _themeMode = ThemeMode.system;
+      }
+      notifyListeners();
+    }
+  }
+
+  void toggleTheme() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
     if (_themeMode == ThemeMode.light) {
       _themeMode = ThemeMode.dark;
+      await prefs.setString('themeMode', 'dark');
     } else {
       _themeMode = ThemeMode.light;
+      await prefs.setString('themeMode', 'light');
     }
     notifyListeners();
   }
